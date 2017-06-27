02c13b9f8f3548e02bea80d0c1c1dcc10fa6a530
Paul Lesur <paul.lesur@plesur.fr>
2017-06-27 20:14:30 +0200

    comment out now broken code (refactoring)

--- config.cpp
+++ config.cpp
@@ -3,18 +3,18 @@
 #include "config.h"
 #include "motor.h"
 
-Motor MainDroite[5];
-Motor Tete[5];
+//Motor MainDroite[5];
+//Motor Tete[5];
 
 int ConfigInmoov::runMotorConfig(int mode)
 {
 /* Initialisation de la MAIN */ 
   
-  MainDroite[1].initServo(3,50,90); //INDEX
-  MainDroite[3].initServo(5,90,120);
+//  MainDroite[1].initServo(3,50,90); //INDEX
+//  MainDroite[3].initServo(5,90,120);
   
-  Tete[0].initServo(12,30,100);
-  Tete[1].initServo(13,60,100);
+//  Tete[0].initServo(12,30,100);
+//  Tete[1].initServo(13,60,100);
 
 /* COUCOU */  
       
@@ -23,7 +23,7 @@ int ConfigInmoov::runMotorConfig(int mode)
 
 int ConfigInmoov::runTEST()
 {
-  delay(1000);
+  /*delay(1000);
   MainDroite[1].setAngle(50);
   delay(1000);
   MainDroite[1].setAngle(90);
@@ -40,7 +40,7 @@ int ConfigInmoov::runTEST()
   delay(1000);
   Tete[1].setAngle(60);
   delay(1000);
-  Tete[1].setAngle(100);
+  Tete[1].setAngle(100);*/
   
   return 0;
 }
