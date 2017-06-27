bb7646f689cd4f09cead2568c272d4d3df85a0cb
Paul Lesur <paul.lesur@plesur.fr>
2017-06-27 20:30:03 +0200

    remove test from the config file, add test from demo file

--- Inmoov_embarque_v1_0.ino
+++ Inmoov_embarque_v1_0.ino
@@ -4,21 +4,24 @@
 */
  #include <Servo.h>
  #include "motor.h"
- #include "config.h"
+// #include "config.h"
+#include "demo.h"
 
 #include "limbs/hand.h"
- 
- ConfigInmoov InmoovROBOT; //POUCE
- 
- 
-// the setup routine runs once when you press reset:
-void setup() {        
-    InmoovROBOT.runMotorConfig(1);
 
-    
+// ConfigInmoov InmoovROBOT; //POUCE
+ Hand rightHand;
+
+
+// the setup routine runs once when you press reset:
+void setup() {
+//    InmoovROBOT.runMotorConfig(1);
+
+
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-InmoovROBOT.runTEST();
+//InmoovROBOT.runTEST();
+	demoHand(rightHand);
 }
