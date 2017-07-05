4a81165744d9e64ea9e4db47573723d6d9161780
Paul Lesur <paul.lesur@plesur.fr>
2017-07-05 22:08:03 +0200

    add reverse attribute and rename methods/attributes to better reflect reality (and english language)

--- motor.h
+++ motor.h
@@ -4,33 +4,29 @@ Page de définition des servo
 
 */
 
-
 #ifndef MOTOR_H
 #define MOTOR_H
 
 #include <Arduino.h>
 #include <Servo.h>
 
-class Motor: public Servo
-{
+class Motor : public Servo {
 
-private:
-  
-  int limitMax;
-  int limitMin;
+      private:
+	bool reverse;
+	int maxAngle;
+	int minAngle;
 
-public: 
-  
-  Motor(int, int=30, int=150); //PIN , Limit Min , Limit Max   
-  void setAngle(int); //Angle
-  
-  void setLimitMax(int);
-  int getLimitMax();
-  
-  void setLimitMin(int);
-  int getLimitMin();
+      public:
+	Motor(int, bool = false, int = 30,
+	      int = 150);   // PIN , Limit Min , Limit Max
+	void setAngle(int); // Angle
+
+	void setMaxAngle(int);
+	int getMaxAngle() const;
+
+	void setMinAngle(int);
+	int getMinAngle() const;
 };
 
-
 #endif /* MOTOR_H */
-
